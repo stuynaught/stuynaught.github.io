270794c9f149229b98eb5cc452dae840812170f0
Yasmeen Roumie <y4smeen@gmail.com>
2015-12-07 20:57:55 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,20 @@ $(document).ready(function(e) {
 		e.stopPropagation();
 	});
 	
+	
+  	$('#slide-1').particleground({
+		dotColor: '#DDDDDD',
+    		lineColor: 'rgba(255, 255, 255, .2)',
+    		particleRadius: 3,
+    		proximity: 80,
+    		minRadiusPct: .4,
+    		maxRadiusPct: 1.8,
+    		minOpacity: .4,
+    		maxOpacity: .8,
+    		parallax: true
+  	});
+
+	
 	/***************
 	* = Hover text *
 	* Hover text for the last slide
